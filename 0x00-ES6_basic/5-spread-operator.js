8708be5d7a68b8694d24285278565f60ff32a82c
MakamuT <makamuthe2@gmail.com>
2024-08-05 18:51:57 +0000

    intro ES6

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  array1 = [...array1, ...array2, string];
-  return array1;
+  const array = [...array1, ...array2, ...string];
+  return array;
 }
