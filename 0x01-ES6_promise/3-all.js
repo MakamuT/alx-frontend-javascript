7cb191a655ab1827c8381ede52ca80b010ebb783
MakamuT <makamuthe2@gmail.com>
2024-08-09 12:12:11 +0000

    promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const [user] = results;
+      const [photo, user] = results;
       console.log(`${user.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
