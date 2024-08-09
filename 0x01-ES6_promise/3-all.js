325057186e3e23ed8b5fda58a2b01b5b12248572
MakamuT <makamuthe2@gmail.com>
2024-08-09 12:16:47 +0000

    promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photo, user] = results;
-      console.log(`${user.body} ${user.firstName} ${user.lastName}`);
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
